388612029e4793eef4594ac9a8ed13641a0f5ee8
Darlan Silva <Darlan Silva@10.0.0.16>
2019-04-13 16:49:35 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/senac/OpenBeer/Application/OpenBeerApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.openBeer.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBeerApplication.class, args);
+	}
+
+}
